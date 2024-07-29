5f29612d4f94858e8e564d0394aa24f2b57f7eda
Adek1n <memeboi5789@gmail.com>
2024-07-29 11:38:51 +0800

    added controllers

--- routes/people.js
+++ routes/people.js
@@ -1,31 +1,10 @@
 import express from "express";
 import {products,people} from "../data.js";
+import {peopleGet,peoplePost,peoplePut} from "../controller/people.js"
 const router=express.Router();
 
-router.post("/api/people",(req,res)=>{
-    const {user}=req.body;
-    if(user){
-        res.status(201).json({
-            sucess:true,
-            code:201
-        });
-    }
-    else return res.status(400).json({
-        sucess:false,
-        code:400,
-        msg:"Please provide a value"
-    });
-
-}) 
-router.put("/api/people",(req,res)=>{
-    res.status(200).json({
-        sucess:true,
-        code:200
-    });
-})
-
-router.get("/",(req,res)=>{
-    res.status(200).json(people);
-})
+router.post("/",peoplePost) 
+router.put("/",peoplePut)
+router.get("/",peopleGet)
 
 export default router;
